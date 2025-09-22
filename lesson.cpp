20424d11842c3376e0fc15aad22583d7598822aa
wooddie <n1ght.d1ck1ns@gmail.com>
2025-09-22 15:48:02 +0500

    medium task

--- lesson.cpp
+++ lesson.cpp
@@ -16,18 +16,50 @@
 #include <fstream>
 #include <ctime>
 #include <bitset>
-
-
+#include <sstream>
+#include <limits>
 
 int main()
 {
-    float n = 0;
-
-    std::cout << "Введите расстояние в верстах: ";
+    int n = 0;
+    std::cout << "Введите количество чисел последовательности: ";
     std::cin >> n;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
+    std::string line;
+    std::vector<double> vec;
 
-    std::cout << n << " верст = " << n * 1.0668 << std::endl;
+    while (vec.size() < n)
+    {
+        std::cout << "Введите последовательность чисел через пробел: ";
+        std::getline(std::cin, line);
+
+        std::istringstream iss(line);
+        double num;
+        while (iss >> num)
+        {
+            vec.push_back(num);
+        }
+    }
+
+    if (vec.size() > n)
+    {
+        vec.resize(n);
+    }
+
+    double sum = 0;
+    for (double n : vec)
+    {
+        sum += n;
+    }
+
+    double min_value = *std::min_element(vec.begin(), vec.end());
+    double max_value = *std::max_element(vec.begin(), vec.end());
+
+    std::cout << std::fixed << std::setprecision(1);
+    std::cout << "Среднее арифметическое: " << sum / vec.size() << std::endl;
+    std::cout << "Минимальное число: " << min_value << std::endl;
+    std::cout << "Максимальное число: " << max_value << std::endl;
 
     return 0;
 }
